7efc21f1bc6598c8d4616fce17d8f5fc0f7698f7
Parker <parkeradamjones@gmail.com>
2019-01-22 18:40:18 -0600

    added the file open and close stuff

--- src/DeRP/Scanner.java
+++ src/DeRP/Scanner.java
@@ -1,11 +1,14 @@
 package DeRP;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 
 
 public class Scanner {
 
     private static File file;
+
+    private static FileInputStream iStream;
 
     public char getChar() {
         return 'a';
@@ -13,8 +16,25 @@ public class Scanner {
 
     public static void main(String[] args) {
 
-        file = new File("program.drp");
+        file = new File("/Users/parker/Desktop/DRP/program.txt");
 
+        try {
+            iStream = new FileInputStream(file);
+
+            //this is where the code for reading each character goes
+
+        }
+        catch(IOException error) {
+            error.printStackTrace();
+        }
+        finally {
+            try {
+                if (iStream != null)
+                    iStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         Lexer lex = new Lexer();
     }
 }
