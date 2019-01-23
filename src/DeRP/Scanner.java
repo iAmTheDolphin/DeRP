20f241d01597099580b0efaba88f87e0825ab31d
Parker <parkeradamjones@gmail.com>
2019-01-22 18:31:02 -0600

    basic stuff buildup

--- src/DeRP/Scanner.java
+++ src/DeRP/Scanner.java
@@ -1,7 +1,20 @@
 package DeRP;
+import java.io.File;
+import java.io.FileInputStream;
+
 
 public class Scanner {
+
+    private static File file;
+
+    public char getChar() {
+        return 'a';
+    }
+
     public static void main(String[] args) {
+
+        file = new File("program.drp");
+
         Lexer lex = new Lexer();
     }
 }
